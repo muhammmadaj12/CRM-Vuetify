e59fea22bec8288a060203ab3daa7a7d96b73ffb
Mohaib-Rehman <144917904+Mohaib-Rehman@users.noreply.github.com>
2023-09-26 05:21:56 -0700

    today i add two button first one where i add...

--- crm-pf/src/views/UserManagment.vue
+++ crm-pf/src/views/UserManagment.vue
@@ -1,8 +1,35 @@
 <template>
     <div>
-        <button id="button1" @click="openForm">Open Form</button>
+        <button @click="showUserForm = true; showRollForm = false">Add User</button>
+        <button @click="showUserForm = false; showRollForm = true">Add Roll</button>
+        <table v-if="showUserForm">
+            <!-- Table headers -->
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
 
-        <!-- User Update Dialog -->
+            <!-- Table body -->
+            <tbody>
+                <tr v-for="(user, index) in users" :key="index">
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>
+                        <div>
+                            <v-btn @click="editUser(user)">
+                                <v-icon>mdi-pencil</v-icon>
+                            </v-btn>
+                            <v-btn @click="confirmDeleteUser(user.id)">
+                                <v-icon>mdi-delete</v-icon>
+                            </v-btn>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
         <v-dialog v-model="editDialog" max-width="400px">
             <!-- ... Existing dialog content ... -->
             <v-card>
@@ -18,185 +45,141 @@
             </v-card>
         </v-dialog>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(user, index) in users" :key="index">
-                    <td>{{ user.name }}</td>
-                    <td>{{ user.email }}</td>
-                    <td>
-                        <div>
-                            <v-btn @click="editUser(user)">
-                                <v-icon>mdi-pencil</v-icon> Edit
-                            </v-btn>
-                            <v-btn @click="confirmDeleteUser(user.id)">
-                                <v-icon>mdi-delete</v-icon> Delete
-                            </v-btn>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
 
-        <!-- Add User Form (Initially hidden) -->
-        <div v-if="showForm">
-            <add-user @user-added="onUserAdded" @close-form="closeForm"></add-user>
-            <v-divider></v-divider>
+        <!-- Add User Form -->
+        <div v-if="showUserForm">
+
+            <form @submit.prevent="addUser">
+                <label for="userName">User Name:</label>
+                <input type="text" v-model="userName" required><br><br>
+
+                <label for="userEmail">User Email:</label>
+                <input type="email" v-model="userEmail" required><br><br>
+                <label>User Role:</label><br>
+                <input type="radio" id="admin" value="admin" v-model="userRole">
+                <label for="admin">Admin</label><br>
+                <input type="radio" id="user" value="user" v-model="userRole">
+                <label for="user">User</label><br>
+                <input type="radio" id="guest" value="guest" v-model="userRole">
+                <label for="guest">Guest</label><br><br>
+
+                <button type="submit">Add User</button>
+            </form>
         </div>
 
-        <!-- Success Snackbar -->
-        <v-snackbar v-model="successSnackbar" :timeout="3000" color="success">
-            {{ successMessage }}
-            <v-btn color="white" text @click="successSnackbar = false">Close</v-btn>
-        </v-snackbar>
+        <!-- Add Roll Form -->
+        <div v-if="showRollForm">
 
-        <!-- Confirm Delete Dialog -->
-        <v-dialog v-model="deleteDialog" max-width="400px">
-            <v-card>
-                <v-card-title>Confirm Delete</v-card-title>
-                <v-card-text>
-                    Are you sure you want to delete this user?
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn color="red" @click="deleteUser(confirmedUserId)">Delete</v-btn>
-                    <v-btn color="blue" @click="cancelDelete">Cancel</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+            <form @submit.prevent="addRoll">
+                <label for="rollName">Roll Name:</label>
+                <input type="text" v-model="rollName" required><br><br>
+
+                <label for="rollDescription">Roll Description:</label>
+                <textarea v-model="rollDescription" rows="4" cols="50"></textarea><br><br>
+
+                <label for="rollPrice">Roll Price:</label>
+                <input type="number" v-model="rollPrice" step="0.01" required><br><br>
+
+                <button type="submit">Add Roll</button>
+            </form>
+        </div>
+
+
     </div>
 </template>
   
-  
 <script>
-import AddUser from "@/components/AddUser.vue";
-
+ 
 export default {
     components: {
-        AddUser,
+
+
     },
     data() {
         return {
             users: [],
-            editDialog: false,
-            editedUser: {
-                name: "",
-                email: "",
-
-            },
-            editedIndex: -1,
-            successSnackbar: false,
-            successMessage: "",
-            deleteDialog: false, // Add delete confirmation dialog data property
-            confirmedUserId: null, // Store the user ID to be deleted
-            showForm: false,
+            showUserForm: false,
+            showRollForm: false,
+            userName: "",
+            userEmail: "",
+            rollName: "",
+            rollDescription: "",
+            rollPrice: 0,
         };
     },
-
-
-    mounted() {
-        this.fetchUsers();
-    },
     methods: {
-        openForm() {
-            // When the button is clicked, toggle the value of showForm
-            this.showForm = !this.showForm;
+        addUser() {
+            // Handle user addition logic here
+            console.log("User Name:", this.userName);
+            console.log("User Email:", this.userEmail);
+            // You can send a request to add a user to your backend here
         },
-        fetchUsers() {
-            fetch("http://127.0.0.1:8000/api/users")
-                .then((response) => response.json())
-                .then((data) => {
-                    this.users = data.data;
-                })
-                .catch((error) => {
-                    console.error("Error fetching users:", error);
-                });
+        addRoll() {
+            // Handle roll addition logic here
+            console.log("Roll Name:", this.rollName);
+            console.log("Roll Description:", this.rollDescription);
+            console.log("Roll Price:", this.rollPrice);
+            // You can send a request to add a roll to your backend here
         },
-        onUserAdded(user) {
-            this.users.push(user);
-            console.log('User Added:', user);
-            this.showForm = false;
-        },
-        closeForm() {
-            // Close the form without adding a user (e.g., cancel button)
-            this.showForm = false;
-        },
-        editUser(user) {
-            this.editedIndex = this.users.indexOf(user);
-            this.editedUser = Object.assign({}, user);
-            this.editDialog = true;
-        },
-        saveUser() {
-            this.successMessage = "User data saved successfully";
-            this.successSnackbar = true;
-            fetch(`http://127.0.0.1:8000/api/users/${123}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(this.editedUser),
-
-            })
-                .then(() => {
-                    this.$set(this.users, this.editedIndex, this.editedUser);
-                    this.cancelEdit();
-
-                })
-                .catch((error) => {
-                    console.error("Error updating user:", error);
-                });
-        },
-        cancelEdit() {
-            this.editDialog = false;
-            this.editedUser = {
-                name: "",
-                email: "",
-            };
-            this.editedIndex = -1;
-        },
-        confirmDeleteUser(user) {
-            this.confirmedUser = user; // Store the user object to be deleted
-            this.deleteDialog = true;
-        },
-        deleteUser(user) {
-            if (user) {
-                const deleteUrl = `http://127.0.0.1:8000/api/users/${user.id}`;
-
-                // Make the DELETE request with the correct URL from the user object
-                fetch(deleteUrl, {
-                    method: "DELETE",
-                })
-                    .then(() => {
-                        // Remove the deleted user from the local users array
-                        this.users = this.users.filter((u) => u.id !== user.id);
-                        this.successMessage = "User data deleted successfully";
-                        this.successSnackbar = true;
-                    })
-                    .catch((error) => {
-                        console.error("Error deleting user:", error);
-                    });
-
-                // Close the delete confirmation dialog
-                this.deleteDialog = false;
+        addUser() {
+            // Validate user inputs if needed
+            if (!this.userName || !this.userEmail) {
+                // Handle validation error (e.g., show an error message)
+                return;
             }
-        },
 
-        confirmDeleteUser(userId) {
-            this.confirmedUserId = userId;
-            this.deleteDialog = true;
-        },
-        cancelDelete() {
-            this.deleteDialog = false;
+            // Create a new user object
+            const newUser = {
+                name: this.userName,
+                email: this.userEmail,
+            };
+
+            // Push the new user to the users array
+            this.users.push(newUser);
+
+            // Clear the form fields
+            this.userName = "";
+            this.userEmail = "";
         },
     },
 };
 </script>
 <style>
+/* CSS for the Add User Form */
+form {
+    margin: 20px;
+}
+
+label {
+    display: block;
+    margin-bottom: 8px;
+}
+
+input[type="text"],
+input[type="email"],
+textarea,
+input[type="number"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+button[type="submit"] {
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button[type="submit"]:hover {
+    background-color: #0056b3;
+}
+
+/* table form css */
 /* Style the table */
 table {
     width: 100%;
@@ -204,6 +187,7 @@ table {
     margin-top: 10px;
 }
 
+/* Style the table headers */
 thead {
     background-color: #f2f2f2;
 }
@@ -215,11 +199,12 @@ td {
     border-bottom: 1px solid #ddd;
 }
 
+/* Style table rows on hover */
 tr:hover {
     background-color: #f5f5f5;
 }
 
-/* Style the buttons within the table */
+/* Style buttons within the table */
 v-btn {
     padding: 6px 12px;
     background-color: #007BFF;
@@ -233,34 +218,4 @@ v-btn {
 v-btn:hover {
     background-color: #0056b3;
 }
-
-#button1 {
-    padding: 10px 20px;
-    /* Adjust padding as needed */
-    background-color: #007BFF;
-    /* Background color */
-    color: #fff;
-    /* Text color */
-    border: none;
-    /* Remove button border */
-    cursor: pointer;
-    margin-top: 10px;
-    margin-left: 0px;
-
-}
-
-#button1 {
-    padding: 10px 20px;
-    background-color: #007BFF;
-    color: #fff;
-    cursor: pointer;
-    margin-left: 850px;
-}
-
-#button1:hover {
-    background-color: #0056b3;
-    /* Background color on hover */
-}
 </style>
-
-
